9f3abf8623504012e6938cc475563639f0575f8a
Donatas Noreika <donoras@gmail.com>
2020-03-15 21:49:56 +0200

    dar

--- many2many_test.py
+++ many2many_test.py
@@ -29,15 +29,45 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-tevas1 = Tevas(vardas="Tėvas", pavarde="Tėvaika")
-tevas2 = Tevas(vardas="Motina", pavarde="Tevienė")
-vaikas1 = Vaikas(vardas="Vaikas", pavarde="Tėvaika")
-vaikas2 = Vaikas(vardas="Vaikė", pavarde="Tėvaikytė")
 
-# Kaip pridėti tėvus ir vaikus
+# Kaip pridėti daug tėvų su daug vaikų
+# tevas1 = Tevas(vardas="Tėvas", pavarde="Tėvaika")
+# tevas2 = Tevas(vardas="Motina", pavarde="Tevienė")
+# vaikas1 = Vaikas(vardas="Vaikas", pavarde="Tėvaika")
+# vaikas2 = Vaikas(vardas="Vaikė", pavarde="Tėvaikytė")
+#
 # tevas1.vaikai.append(vaikas1)
 # tevas2.vaikai.append(vaikas1)
 # tevas2.vaikai.append(vaikas2)
+#
 # session.add(tevas1)
 # session.add(tevas2)
 # session.commit()
+
+# Kaip gauti visus tėvo vaikus:
+
+# tevas = session.query(Tevas).get(2)
+# for vaikas in tevas.vaikai:
+#     print(vaikas.vardas, vaikas.pavarde)
+
+# Kaip gauti visus vaiko tėvus:
+
+# vaikas = session.query(Vaikas).get(1)
+# for tevas in vaikas.tevai:
+#     print(tevas.vardas, tevas.pavarde)
+
+# Kaip pakeisti tėvo vaiko įrašą:
+tevas = session.query(Tevas).get(2)
+tevas.vaikai[0].vardas = "Vaikas 1"
+session.commit()
+
+# Kaip ištrinti tėvo vaiko įrašą:
+
+tevas = session.query(Tevas).get(2)
+vaikas1 = tevas.vaikai[0]
+tevas.vaikai.remove(vaikas1)
+session.commit()
+
+tevas = session.query(Tevas).get(2)
+for vaikas in tevas.vaikai:
+    print(vaikas.vardas, vaikas.pavarde)
